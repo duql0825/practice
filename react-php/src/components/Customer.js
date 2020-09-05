a5a2c195d1ae1d202e9c8c66e744b67681f5798d
duql0825 <duql0825@naver.com>
2020-09-05 19:34:15 +0900

    누락된 파일들 업로드 (react-php폴더)

---
+++ react-php/src/components/Customer.js
@@ -0,0 +1,36 @@
+import React from "react";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import CustomerDelete from "./CustomerDelete";
+import { withStyles } from "@material-ui/core/styles";
+
+const styles = theme => ({
+  image: {
+    maxWidth: 100
+  }
+});
+
+// 고객 정보도 이렇게 객체 하나하나로 자잘하게 나누어 주는게 중요하다.
+class Customer extends React.Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <TableRow>
+        <TableCell>{this.props.id}</TableCell>
+        <TableCell>
+          <img className={classes.image} src={this.props.image} alt="profile" />
+        </TableCell>
+        <TableCell>{this.props.name}</TableCell>
+        <TableCell>{this.props.type}</TableCell>
+        <TableCell>
+          <CustomerDelete
+            stateRefresh={this.props.stateRefresh}
+            id={this.props.id}
+          />
+        </TableCell>
+      </TableRow>
+    );
+  }
+}
+
+export default withStyles(styles)(Customer);
---
+++ react-php/src/components/CustomerAdd.js
@@ -0,0 +1,163 @@
+import React from "react";
+import { post } from "axios";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import { withStyles } from "@material-ui/core/styles";
+
+const styles = (theme) => ({
+  hidden: {
+    display: "none",
+  },
+});
+
+class CustomerAdd extends React.Component {
+  // 선언 하듯이 넣으세요
+  constructor(props) {
+    super(props);
+    this.state = {
+      file: null,
+      userName: "",
+      type: "",
+      fileName: "",
+      open: false,
+    };
+  }
+
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    this.addCustomer().then((response) => {
+      console.log(response.data);
+      this.props.stateRefresh();
+    });
+    this.setState({
+      file: null,
+      userName: "",
+      type: "",
+      fileName: "",
+      open: false,
+    });
+  };
+
+  handleFileChange = (e) => {
+    this.setState({
+      file: e.target.files[0],
+      fileName: e.target.value,
+    });
+  };
+
+  handleValuechange = (e) => {
+    let nextState = {};
+    nextState[e.target.name] = e.target.value;
+    this.setState(nextState);
+  };
+
+  addCustomer = () => {
+    const url = "/api/customers";
+    const formData = new FormData();
+    formData.append("image", this.state.file);
+    formData.append("name", this.state.userName);
+    formData.append("type", this.state.type);
+    const config = {
+      headers: {
+        "content-type": "multipart/form-data",
+      },
+    };
+    return post(url, formData, config);
+  };
+
+  handleClickOpen = () => {
+    this.setState({
+      open: true,
+    });
+  };
+
+  handleClose = () => {
+    this.setState({
+      file: null,
+      userName: "",
+      type: "",
+      fileName: "",
+      open: false,
+    });
+  };
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <div>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={this.handleClickOpen}
+        >
+          고객추가하기
+        </Button>
+        <Dialog open={this.state.open} onClose={this.handleClose}>
+          <DialogTitle>고객 추가</DialogTitle>
+          <DialogContent>
+            <TextField
+              className={classes.hidden}
+              accept="image/*"
+              id="raised-button-file"
+              type="file"
+              name="file"
+              file={this.state.file}
+              value={this.state.fileName}
+              onChange={this.handleFileChange}
+            />{" "}
+            <label htmlFor="raised-button-file">
+              <Button
+                variant="contained"
+                color="primary"
+                component="span"
+                name="file"
+              >
+                {this.state.fileName === ""
+                  ? "프로필 이미지 선택"
+                  : this.state.fileName}
+              </Button>
+            </label>
+            <br />
+            <TextField
+              label="이름"
+              type="text"
+              name="userName"
+              value={this.state.userName}
+              onChange={this.handleValuechange}
+            />{" "}
+            <br />
+            <TextField
+              label="타입"
+              type="text"
+              name="type"
+              value={this.state.type}
+              onChange={this.handleValuechange}
+            />{" "}
+          </DialogContent>
+          <DialogActions>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={this.handleFormSubmit}
+            >
+              추가
+            </Button>
+            <Button
+              variant="contained"
+              color="outline"
+              onClick={this.handleClose}
+            >
+              닫기
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles)(CustomerAdd);
